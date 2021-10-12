9e5f873908efb9900c9d63f6c9265cf97894c09a
César Téllez <carodrigueztellez@ucundinamarca.edu.co>
2021-10-12 12:06:21 -0500

    Noticias de ejemplo y path del registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material/material.module';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewsComponent } from './pages/home/news/news.component';
+import { LoginComponent } from './pages/home/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    HomeComponent,
+    NewsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
