bf08e371f32aee00f2278766a20c21c6ceb2a13e
César Téllez <carodrigueztellez@ucundinamarca.edu.co>
2021-10-17 01:21:28 -0500

    Preferencias listas

--- src/app/pages/preferences/categories/categories.component.ts
+++ src/app/pages/preferences/categories/categories.component.ts
@@ -10,24 +10,25 @@ import { AppComponent } from 'src/app/app.component';
 })
 export class CategoriesComponent implements OnInit {
   state: boolean = false;
+  selectedValue = []
+
   categories = [
-    {name: "Categoría 1"},
-    {name: "Categoría 2"},
-    {name: "Categoría 3"},
-    {name: "Categoría 4"},
-    {name: "Categoría 5"},
-    {name: "Categoría 6"},
-    {name: "Categoría 7"},
-    {name: "Categoría 8"},
-    {name: "Categoría 9"},
-    {name: "Categoría 10"},
-    {name: "Categoría 11"},
-    {name: "Categoría 12"}
+    {"id": 1, "name": "Categoria 1"},
+    {"id": 2, "name": "Categoria 2"},
+    {"id": 3, "name": "Categoria 3"},
+    {"id": 4, "name": "Categoria 4"},
+    {"id": 5, "name": "Categoria 5"},
+    {"id": 6, "name": "Categoria 6"},
+    {"id": 7, "name": "Categoria 7"},
+    {"id": 8, "name": "Categoria 8"},
+    {"id": 9, "name": "Categoria 9"},
+    {"id": 10, "name": "Categoria 10"},
+    {"id": 11, "name": "Categoria 11"},
+    {"id": 12, "name": "Categoria 12"}
   ];
 
   constructor(
-    home: AppComponent,
-    private messageService: MessageService
+    home: AppComponent
   ){
     home.state = this.state;
   }
