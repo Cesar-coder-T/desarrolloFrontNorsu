f83f5ac31879fabdec3248f6e720ebafba73fa82
CesarTllez <lcesartellezl@gmail.com>
2021-12-16 20:36:58 -0500

    Ajustes pagina inicio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,17 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  goDown(){
+  goDown() {
     window.scroll(0, 649);
   }
-
 }
