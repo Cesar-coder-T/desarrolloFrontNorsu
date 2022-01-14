f09e735daeed01900cae1b6313466b3166843b3d
CesarTllez <lcesartellezl@gmail.com>
2022-01-13 21:26:09 -0500

    Ajustes

--- src/app/pages/collaborator/collaborator.component.ts
+++ src/app/pages/collaborator/collaborator.component.ts
@@ -7,17 +7,24 @@ import { Component } from '@angular/core';
 })
 export class CollaboratorComponent {
 
+  stateSklt: boolean;
+
+  /*Remove this when the API implemented*/
+  stateInfo: boolean;
+  /*Remove this when the API implemented*/
+
   /*SideNav App options*/
   options: any;
 
   constructor() {
+    this.stateSklt = true;
+
+    /*Remove this when the API implemented*/
+    this.stateInfo = false;
+    /*Remove this when the API implemented*/
+
     /*SideNav App options*/
     this.options = [
-      {
-        icon: 'account_circle',
-        title: 'Mi Perfil',
-        route: '/colaborador/mi-perfil',
-      },
       {
         icon: 'play_lesson',
         title: 'Cursos',
@@ -27,6 +34,11 @@ export class CollaboratorComponent {
         icon: 'library_books',
         title: 'Certificados',
         route: '/colaborador/mis-certificados',
+      },
+      {
+        icon: 'account_circle',
+        title: 'Mi Perfil',
+        route: '/colaborador/mi-perfil',
       }
     ];
     /*End sideNav App options*/
